08d26f060173a2b1d222c72ed0ef41d708cece61
Ye-Cai <ye.cai@thoughtworks.com>
2021-10-31 18:48:40 +0800

    [caiye] feat: happy path for js homework-01.

--- js/chapter-one/homework/practise.js
+++ js/chapter-one/homework/practise.js
@@ -1,23 +1,25 @@
 //问题1
 const removeRepeat = () => {
     const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
-    // todo
+    const a = array.filter((v, i, arr) => arr.indexOf(v) === i);
+    console.log(a);
     // 输出[1, 2, 3, "a", "b"]
 }
 removeRepeat();
 
 //问题2
-const findRepeat= () => {
-    const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
-    // todo
+const findRepeat = () => {
+    const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b']
+    const a = array.filter((v, i, arr) => arr.indexOf(v) !== i);
+    console.log(new Set(a));
     // 输出[2, 3, "a"]
 }
 findRepeat();
 
 
 //问题3
-const clone= () => {
-    //todo
+const clone = (obj) => {
+    return JSON.parse(JSON.stringify(obj));
 }
 const obj = {
     arr: [{a: 1, f: {q: 1}}, 2, 3],
@@ -29,14 +31,13 @@ console.log(obj1.string === obj.string);
 
 
 //问题4
-const question4= () => {
+const question4 = () => {
     for (var i = 0; i < 5; i++) {
-        setTimeout(function() {
-            console.log(i);
+        setTimeout(function (a) {
+            console.log(a);
         }, 1000);
     }
     console.log(i)
-    // todo
     //修改代码输出 5,0,1,2,3,4
 }
 question4();
